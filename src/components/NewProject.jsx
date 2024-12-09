7a945239504205a7a813312b45c59ba1ac01cbe3
delewska <121955254+delewska@users.noreply.github.com>
2024-12-09 16:54:03 +0000

    add inputs styling with Tailwind

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -4,10 +4,11 @@ import Input from "./Input.jsx";
 
 export default function NewProject() {
   return (
+    //[35rem] is a special class in tailwind
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
         <li>
-          <button className="text-stone-800 hover:text-stone-9500">
+          <button className="text-stone-800 hover:text-stone-950">
             Cancel
           </button>
         </li>
