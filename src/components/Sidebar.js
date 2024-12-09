d744545ddfba94beebdbf46e7cd95dbdebf935cb
delewska <121955254+delewska@users.noreply.github.com>
2024-12-09 12:52:33 +0000

    add Sidebar for add new projects

---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <aside>
+      <h1>Your Projects</h1>
+      <div>
+        <button>Add Project</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
