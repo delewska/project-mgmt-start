9df4c5f84355489da39946e813e2bb3183f97a42
delewska <121955254+delewska@users.noreply.github.com>
2024-12-10 09:24:12 +0000

    Splitting Components to Split JSX & Tailwind Styles ( for Higher Reusably)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 import Sidebar from "./components/Sidebar.jsx";
+//delete only to check the EntryPage
 import NewProject from "./components/NewProject.jsx";
+import EntryPage from "./components/EntryPage.jsx";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
-      <NewProject />
+      <EntryPage />
     </main>
   );
 }
---
+++ src/components/EntryPage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Button from "./Button.jsx";
+import noProjects from "../assets/no-projects.png";
+
+export default function EntryPage() {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img
+        src={noProjects}
+        alt="image of notebook with pen"
+        className="w-16 h-16 object-contain mx-auto"
+      />
+      <h2 className="text-xl font text-stone-500 my-4">No Project Selected</h2>
+      <p className="text-stone-400 mb-4">
+        Select a project or get started with a new one
+      </p>
+      <p className="mt-8">
+        <Button>Create new project</Button>
+      </p>
+    </div>
+  );
+}
