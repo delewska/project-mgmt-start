1f9ed74e12ec7f6f937a4abd22f0bb6df66aea8c
delewska <121955254+delewska@users.noreply.github.com>
2024-12-10 17:50:30 +0100

    Add projectsState to App komponent to control adding projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 
 import Sidebar from "./components/Sidebar.jsx";
-//delete only to check the EntryPage
 import NewProject from "./components/NewProject.jsx";
 import EntryPage from "./components/EntryPage.jsx";
 
 function App() {
+const [projectsState, setProjectsState] = React.useState({
+  selectedProjectId: undefined,
+  projects: []
+})
+
+const handleAddNewProject = () => {
+  setProjectsState(prevState => {
+  return {
+    ...prevState,
+    selectedProjectId: null,
+    };
+  });
+}
+  
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <EntryPage />
+      <Sidebar onAddNewProject={handleAddNewProject} />
+      <EntryPage onAddNewProject={handleAddNewProject} />
     </main>
   );
 }
