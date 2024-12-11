3546f95f4913af608767d3f60ab87f1031f902b2
delewska <natalia.delewskaa@gmail.com>
2024-12-11 13:20:23 +0100

    fix bugs with curly brakets

--- src/App.jsx
+++ src/App.jsx
@@ -5,47 +5,51 @@ import NewProject from "./components/NewProject.jsx";
 import EntryPage from "./components/EntryPage.jsx";
 
 function App() {
-const [projectsState, setProjectsState] = React.useState({
-  selectedProjectId: undefined,
-  projects: []
-})
-
-const handleAddNewProject = () => {
-  setProjectsState(prevState => {
-  return {
-    ...prevState,
-    selectedProjectId: null,
-    };
+  const [projectsState, setProjectsState] = React.useState({
+    selectedProjectId: undefined,
+    projects: [],
   });
-}
+
+  const handleAddNewProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  };
 
   const handleAddProject = (projectData) => {
-  setProjectsState(prevState => {
-    const projectId = Math.random();
-    const newProject = {
-      ...projectsData,
-      id: projectId,
-    return {
-      ...prevState,
-      selectedProjectId: undefined,
-      projects: [...prevState.projects, newProject]
-    }
-  }
-  }
-
-console.log("projectData: ", projectData);
-  
-let content; 
-
-  if(projectsState.selectedProjectId === null) {
-    content = <NewProject onAddProject={handleAddProject) />
-  } else if(projectsState.selectedProjectId === undefined) {
-    content = <EntryPage onAddNewProject={handleAddNewProject} />
+    setProjectsState((prevState) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
   };
-  
+
+  console.log("projectData: ", projectData);
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAddProject={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <EntryPage onAddNewProject={handleAddNewProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar onAddNewProject={handleAddNewProject} projcts={projectsStates.projects} />
+      <Sidebar
+        onAddNewProject={handleAddNewProject}
+        projcts={projectsStates.projects}
+      />
       {content}
     </main>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Button from "./Button.jsx";
 
-export default function Sidebar({onAddNewProject, projects}) {
+export default function Sidebar({ onAddNewProject, projects }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
@@ -12,12 +12,13 @@ export default function Sidebar({onAddNewProject, projects}) {
         <Button onClick={onAddNewProject}>Add Project</Button>
       </div>
       <ul>
-        {projects.map(project => 
+        {projects.map((project) => (
           <li key={project.id}>
             <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:bg-stone-800">
               {project.title}
             </button>
-          </li>)
+          </li>
+        ))}
       </ul>
     </aside>
   );
