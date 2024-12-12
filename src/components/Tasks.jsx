81b8bb0dce8ff9e60180ba48b6f8d41e633e4822
delewska <natalia.delewskaa@gmail.com>
2024-12-12 11:16:30 +0100

    add component Tasks

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,10 @@
+export default function Tasks() {
+  return (
+    <section>
+      <h2>TASKS</h2>
+      NEW TASK
+      <p>This project does not have any tasks yet.</p>
+      <ul></ul>
+    </section>
+  );
+}
