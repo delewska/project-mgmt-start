21bfba8d9930b4fabda7747126f6a56ed076b1cc
delewska <natalia.delewskaa@gmail.com>
2024-12-12 11:23:28 +0100

    use NewTask in Tasks component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,8 +1,10 @@
+import NewTask from "./NewTask";
+
 export default function Tasks() {
   return (
     <section>
-      <h2 className="text-2xl font-bold text-stone-700 mb-4">TASKS</h2>
-      NEW TASK
+      <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+      <NewTask />
       <p className="text-stone-800 mb-4">
         This project does not have any tasks yet.
       </p>
