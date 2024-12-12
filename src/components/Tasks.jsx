bf25b398e3183c86c2643c56b084063a88cde901
delewska <natalia.delewskaa@gmail.com>
2024-12-12 15:18:52 +0100

    add task mapping to managing tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,15 +2,26 @@ import React from "react";
 
 import NewTask from "./NewTask";
 
-export default function Tasks({ onAdd, onDelete }) {
+export default function Tasks({ tasks, onAdd, onDelete }) {
   return (
     <section>
       <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
       <NewTask onAdd={onAdd} onDelete={onDelete} />
-      <p className="text-stone-800 mb-4">
-        This project does not have any tasks yet.
-      </p>
-      <ul></ul>
+      {tasks.length === 0 && (
+        <p className="text-stone-800 mb-4">
+          This project does not have any tasks yet.
+        </p>
+      )}
+      {tasks.length > 0 && (
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              <span>{task.text}</span>
+              <button>Clear</button>
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 }
